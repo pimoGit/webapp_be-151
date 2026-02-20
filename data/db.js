59e20d85cdbb4e7783ec68458d826abcf6848554
Simone <info@simoneicardi.com>
2026-02-20 11:34:16 +0100

    setup env vars

--- data/db.js
+++ data/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '7bloodYr00T',
-    database: 'book'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
