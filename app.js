117f94578b2a28e48ece041193e136ca93aaab90
Simone <info@simoneicardi.com>
2026-02-20 10:56:31 +0100

    setup BE base con middleware di errore

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// import del middelware di gestione errore interno 500
+const errorsHandler = require("./middlewares/errorsHandler");
+// import del middelware di gestione di rotta inesistente
+const notFound = require("./middlewares/notFound");
+
+// importa router delle pizze
+// const pizzasRouter = require('./routers/pizzas');
+
+// attivazione della cartella public per uso file statici
+app.use(express.static('public'));
+
+// registro il body-parser per "application/json"...
+// app.use(express.json());
+
+// registro middleware checkTime
+// app.use(checkTime);
+
+// rotta home APP
+app.get('/', (req, res) => {
+    res.send("<h1>Rotta di home della nostra App dei libri</h1>")
+})
+
+// registrazione middleware su router specifico
+// app.use("/pizzas", checkTime)
+
+// istanza delle rotte per risorsa pizze
+// app.use("/pizzas", pizzasRouter)
+
+// registriamo middelware di gestione err 500
+app.use(errorsHandler);
+
+// registriamo middelware di gestione rotta inesistente
+app.use(notFound);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
