c4826b5ad226ddb756615ce3fb074c0ea77bbd3a
Simone <info@simoneicardi.com>
2026-02-20 12:31:55 +0100

    completa show book con reviews

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -22,15 +22,29 @@ function show(req, res) {
     // prepariamo la query per la richiesta
     const bookSql = 'SELECT * FROM books WHERE id = ?';
 
-    connection.query(bookSql, [id], (err, results) => {
+    const reviewsSql = 'SELECT * FROM reviews WHERE book_id = ?';
+
+    // chiamata a DB principale per recuperare il libro
+    connection.query(bookSql, [id], (err, bookResults) => {
         if (err) return res.status(500).json({ error: 'Database query failed' });
-        if (results.length === 0) return res.status(404).json({ error: 'Book not found' });
+        if (bookResults.length === 0) return res.status(404).json({ error: 'Book not found' });
 
         // salviamo il risultato in una cost
-        const book = results[0];
+        const book = bookResults[0];
 
-        // ritorniamo il json del libro
-        res.json(book);
+        // chiamata a DB secondaria per recupero reviews del libro
+        connection.query(reviewsSql, [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ error: 'Database query failed' });
+
+            // saviamo le reviews in una cost
+            const reviewsArr = reviewsResults;
+
+            // aggiungiamo a oggetto book la prop per le reviews
+            book.reviews = reviewsArr;
+
+            // ritorniamo il json del libro
+            res.json(book);
+        });
     });
 }
 
