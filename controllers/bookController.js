6562e1d4b06b0a8648662508bd5216b3a7c9ceac
Simone <info@simoneicardi.com>
2026-02-20 11:42:12 +0100

    crea controller book base

---
+++ controllers/bookController.js
@@ -0,0 +1,14 @@
+// importiamo la connessione del DB
+const connection = require('../data/db');
+
+// funzione di index
+function index(req, res) {
+    // fai cose
+}
+
+// funzione di show
+function show(req, res) {
+    // fai cose
+}
+
+module.exports = { index, show }
